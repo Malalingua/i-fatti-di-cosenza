82e7104bc5e48f53a3b2e601f8e0770246afeb16
Lorenzo Curia <lorcur.job@gmail.com>
2026-09-24 20:28:52 +0200

    revert: remove CosentineTranslator (API requires paid credits)

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,6 @@ import {
 import { Hero } from '@/components/Hero'
 import { BriefCard } from '@/components/BriefCard'
 import { OtherNewsCard } from '@/components/OtherNewsCard'
-import { CosentineTranslator } from '@/components/CosentineTranslator'
 import { sortCategoriesEditorially, selectLead, splitBriefs, pickOtherNews } from '@/lib/homepage'
 import type { ArticleSummary } from '@/lib/sanity/types'
 
@@ -42,7 +41,7 @@ export default async function HomePage() {
   }
 
   const { top: topBriefs } = splitBriefs(slots, finalBriefs, lead._id)
-  const otherNews = pickOtherNews(latest, [lead, ...topBriefs], 3)
+  const otherNews = pickOtherNews(latest, [lead, ...topBriefs], 4)
 
   return (
     <main className="mx-auto max-w-6xl px-6 py-8">
@@ -61,9 +60,6 @@ export default async function HomePage() {
             {otherNews.map((article) => (
               <OtherNewsCard key={article._id} article={article} />
             ))}
-            <div>
-              <CosentineTranslator />
-            </div>
           </div>
         </section>
       )}
